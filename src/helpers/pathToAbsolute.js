5d92d17a00b401b6cf61955dca6999368a16fe72
Dmitry Agurkov <munnharpa@gmail.com>
2022-12-19 13:26:48 +0300

    path processing fixed

--- src/helpers/pathToAbsolute.js
+++ src/helpers/pathToAbsolute.js
@@ -4,8 +4,6 @@ export default (cwd, pathToCheck) => {
         const { root } = path.parse(cwd);
 
         if (pathToCheck[1] === ':') {return pathToCheck}
-           
-        console.log({root, cwd, pathToCheck,abs: path.isAbsolute(pathToCheck), return: path.join(cwd, pathToCheck) })
 
         return path.isAbsolute(pathToCheck)
          ? path.join(root, pathToCheck)
